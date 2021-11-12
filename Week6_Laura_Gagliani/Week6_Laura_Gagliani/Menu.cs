5ff81119007b449a2d95118cb38fb207b12260e9
Laura Gagliani <gaglianilaura@gmail.com>
2021-11-12 09:21:20 +0100

    Aggiungere i file di progetto.

---
+++ Week6_Laura_Gagliani/Week6_Laura_Gagliani/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week6_Laura_Gagliani
+{
+    class Menu
+    {
+        internal static void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Week6_Laura_Gagliani/Week6_Laura_Gagliani/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Week6_Laura_Gagliani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            /*
+             * Domande Test
+             * 
+             * 1) a, e, g
+             * 2) b, d
+             * 3) a, b, c ?????
+             * 
+          */
+            Menu.Start();
+        }
+    }
+}
